fc2bfbe8961b402a9dec2f8055cbecf99d4fef11
Yunyun <celeph4is@gmail.com>
2020-09-03 14:07:18 -0700

    initial commmit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { ApolloServer, gql } = require('apollo-server');
+let books = require('./data/booksData')
+let authors = require('./data/authorsData')
+
+/*
+ * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
+ * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
+*/
+
+const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: String!
+    genres: [String!]!
+  }
+  type Author {
+    name: String!
+    id: String!
+    born: Int
+  }
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+  }
+`
+
+const resolvers = {
+  Query: {
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
